8f045506b6ac984a5bd1c71ccf994dbf588c1e60
Kappamalone <uzman.zawahir1@gmail.com>
2026-04-17 14:58:32 -0700

    add async git push to keybindings - also meta!

--- config/nvim/lua/plugins/vim-fugitive.lua
+++ config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
+    lazy = false,
     keys = {
       {"<leader>gg", "<cmd>Git<cr>", desc="Fugitive: git status"},
       {"<leader>gd", "<cmd>Gvdiffsplit HEAD<cr>", desc="Fugitive: diff file vs HEAD"},
@@ -9,6 +10,7 @@ return {
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Fugitive: Git blame" },
       { "<leader>gc", "<cmd>Git commit<cr>", desc = "Fugitive: Git commit" },
       { "<leader>gS", "<cmd>vertical Git diff --staged<cr>", desc = "Fugitive: Git diff --staged" },
+      { "<leader>gp", "<cmd>Dispatch git push<cr>", desc = "Fugitive: Git push (async)" },
     },
     config = function(_, opts)
       -- clangd notification spams without this
@@ -69,10 +71,18 @@ return {
         end,
       })
       
-      -- start us off in insertion mode when writing a git commit
+      -- start us off in insertion mode when writing a git commit, unless we're rewording one (test)
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "gitcommit",
         callback = function()
+          -- Check whether there is any non-comment content
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
+            if not line:match("^%s*#") and not line:match("^%s*$") then
+              return -- reword/amend: do NOT enter insert
+            end
+          end
+
+          -- Fresh commit: enter insert mode
           vim.cmd("startinsert")
         end,
       })
